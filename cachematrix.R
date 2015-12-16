dab966b78ee03dbb6416b4c8286b6691c41ae51e
charlesjhlee <charlesjhlee@gmail.com>
2015-12-16 22:29:03 +1100

    R_Programming Assignment 2 - Create and Store Inverse Matrix into Cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#R_Programming
+#Assignment 2
+
+## The functions listed here are used to calculate an inverse of a matrix
+## and store it in a cache (i.e. in a separate environment).
+## Thereafter, upon calling a function to calculate inverse of a matrix,
+## it is checked whether there is inverse matrix already calculated and
+## saved in cache. If the inverse matrix exists in the cache, then this
+## inverse matrix is called, otherwise inverse of matrix is calculated.
+
+
+
+
+## The below function is used to store 4 functions. Set stores the original matrix
+## into cache, get calles on the original matrix, setinvmatrix stores
+## inverse of the matrix in the cache, and getinvmatrix calls on the
+## inverse of the matrix from cache if it exists.
 
 makeCacheMatrix <- function(x = matrix()) {
         im <- NULL
@@ -18,7 +32,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
+## The below function is used to first check whether the inverse matrix exists
+## in the cache. If it exists, then the function simply calls on this inverse
+## matrix from cache and the function stops. If the inverse matrix does not exist
+## then the inverse of the matrix is calculated and the results are returned.
 
 cacheSolve <- function(x, ...) {
         im <- x$getinvmatrix()
@@ -30,5 +48,4 @@ cacheSolve <- function(x, ...) {
         im <- solve(data, ...)
         x$setinvmatrix(im)
         im
-        ## Return a matrix that is the inverse of 'x'
 }
